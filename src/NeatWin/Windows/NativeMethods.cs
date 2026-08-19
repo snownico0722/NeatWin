42a8e652002d1b59ce89f479ae5419ceea30f774
snownico0722 <36026283+snownico0722@users.noreply.github.com>
2026-08-19 18:13:13 +0800

    support configurable hotkey modifiers

--- src/NeatWin/Windows/NativeMethods.cs
+++ src/NeatWin/Windows/NativeMethods.cs
@@ -22,6 +22,8 @@ internal static class NativeMethods
     internal const uint SwpAsyncWindowPos = 0x4000;
     internal const int WmHotkey = 0x0312;
     internal const uint ModAlt = 0x0001;
+    internal const uint ModControl = 0x0002;
+    internal const uint ModShift = 0x0004;
     internal const uint ModWin = 0x0008;
     internal const uint ModNoRepeat = 0x4000;
     internal static readonly nint HwndMessage = new(-3);
